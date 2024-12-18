cbe73e7e13ac6aa6674a0874f8a181e54272d02d
Ulrich Feindt <ufeindt@gmail.com>
2024-12-18 15:05:14 +0100

    Day 18 (binary search)

--- 18/puzzle_2.py
+++ 18/puzzle_2.py
@@ -6,19 +6,26 @@ def solve(
 ) -> tuple[int, int] | None:
     falling_bytes = load_input(filename=filename)
     path = find_path(falling_bytes, goal, n_fallen)
-    n = 1
-    for k in range(n_fallen + 1, len(falling_bytes)):
-        if (next_byte := falling_bytes[k - 1]) in path:
-            path = find_path(falling_bytes, goal, k)
-            n += 1
+    min_n = n_fallen + 1
+    max_n = len(falling_bytes) - 1
+    while max_n != min_n:
+        n_fallen = (max_n + min_n) // 2
+        path = find_path(falling_bytes, goal, n_fallen)
 
-        if not path:
-            print(n)
-            return next_byte
+        if max_n == min_n + 1:
+            if path:
+                return falling_bytes[n_fallen]
+            else:
+                return falling_bytes[n_fallen - 1]
+
+        if path:
+            min_n = n_fallen
+        else:
+            max_n = n_fallen
 
 
 def main():
-    # assert solve(filename="18/test_input", goal=(6, 6), n_fallen=12) == (6, 1)
+    assert solve(filename="18/test_input", goal=(6, 6), n_fallen=12) == (6, 1)
     print(solve())
 
 
